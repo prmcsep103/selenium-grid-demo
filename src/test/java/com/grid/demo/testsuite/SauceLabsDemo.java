bb651da8fb921158f7968511b66caf3d2feccc09
prmcsep103 <primecommunity103@gmail.com>
2023-12-23 17:19:18 +0000

    Grid Demo update

--- src/test/java/com/grid/demo/testsuite/SauceLabsDemo.java
+++ src/test/java/com/grid/demo/testsuite/SauceLabsDemo.java
@@ -1,11 +1,16 @@
 package com.grid.demo.testsuite;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Jay
@@ -16,6 +21,18 @@ public class SauceLabsDemo {
 
     @BeforeMethod
     public void setUp() throws MalformedURLException {
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 11");
+        browserOptions.setBrowserVersion("119");
+        Map<String, Object> sauceOptions = new HashMap<>();
+        sauceOptions.put("username", "oauth-primecommunity103-36640");
+        sauceOptions.put("accessKey", "e61981c7-7887-4269-b369-09f6976bb57a");
+        sauceOptions.put("build", "nop commerce 1");
+        sauceOptions.put("name", "Login Test");
+        browserOptions.setCapability("sauce:options", sauceOptions);
+
+        URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+        driver = new RemoteWebDriver(url, browserOptions);
 
     }
 
